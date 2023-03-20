2fa46be7b1a24c3600ef65b68a4963c7878280a9
nemlic <nemlicmain22@gmail.com>
2023-03-20 12:57:38 +0000

    second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,29 +4,28 @@
 
 /* more headers goes there */
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 /* betty style doc for function main goes there */
-	int main(void)
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+if (n > 0)
 	{
-	 printf("%i is positive\n", n);
+		 printf("%i is positive\n", n);
 	}
-	else if (n == 0)
+else if (n == 0)
 	{
-	printf("%i is zero\n", n);
+		printf("%i is zero\n", n);
 	}
-	else if (n < 0)
+else if (n < 0)
 	{
-	printf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }
