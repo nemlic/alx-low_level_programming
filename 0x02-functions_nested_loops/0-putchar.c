1fbe32b0f5314cd9d17517d2067840e5ec1cf1ce
nemlic <nemlicmain22@gmail.com>
2023-03-21 21:05:16 +0000

    yote

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main- a program that prints _putchar, followed by a new line.
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
