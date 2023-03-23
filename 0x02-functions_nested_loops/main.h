a40bb925030fada608c168103f1fb64dc81a838c
nemlic <nemlicmain22@gmail.com>
2023-03-23 22:03:19 +0000

    peer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,13 @@
-#ifndef main_h
-#define main_h
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
 
 int _putchar(char c);
+
+#endif
