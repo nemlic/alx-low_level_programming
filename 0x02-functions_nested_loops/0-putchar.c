9c4e62e18c7804402afddbb8156d71a44c20463f
nemlic <nemlicmain22@gmail.com>
2023-03-23 21:29:52 +0000

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
   *main- a program that prints _putchar, followed by a new line.
   *Return: Always 0 (Success)
@@ -6,7 +6,15 @@
 
 int main(void)
 {
-	puts("_putchar");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
