11f04ad82861bfe829c997057914173d18de193b
nemlic <nemlicmain22@gmail.com>
2023-03-26 05:44:08 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 	int a;
 	char b;
 
-	for (a = '0'; a <= '14'; a++;)
+	for (a = 0; a <= 14; a++;)
 	{
-		for (b = '0'; b <= 14; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b / 10 > 0)
 				_putchar((b / 10) + '0');
