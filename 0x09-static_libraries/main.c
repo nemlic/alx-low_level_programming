55062998c0599ae3dfb88a8a5650318445a814db
nemlic <nemlicmain22@gmail.com>
2023-04-10 13:42:03 -0700

    corrections

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints out the input
+ * main: where program starts
+ * Return: 0
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-Kevin Mitnick");
+	return (0);
+}
