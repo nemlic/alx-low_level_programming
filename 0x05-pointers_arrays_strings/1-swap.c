63fc866f8d5d2cf89118acfb100dd11cee037922
nemlic <nemlicmain22@gmail.com>
2023-04-14 10:59:38 +0300

    firzt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers values
+ * @a: value to be swaped with b
+ * @b: value to be swapped with a
+ * Return : void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p= *a;
+	*a = *b;
+	*b = p;
+}
