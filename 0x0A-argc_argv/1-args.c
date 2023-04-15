989697647caf1463134a410505fbdd6a13f59bc2
nemlic <nemlicmain22@gmail.com>
2023-04-15 22:18:59 +0000

    yote

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,12 @@
  * main - prints number of arguments passed 
  * @argc: argument count
  * @argv: array pointer to a string
- * Return: 0 on sucess
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-	void argv;
+	(void) argv;
 	printf("%d\n", argc-1);
 
 	return (0);
