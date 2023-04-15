7eb9e8c776dbf7f977c059164a1c8a7924a67e4e
nemlic <nemlicmain22@gmail.com>
2023-04-15 08:45:22 +0000

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - prints name of program
+  *@argc: argument counts
+  *@argv: pointer to array of strings
+  *Return: 0 on sucess
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0])
+	}
+}
