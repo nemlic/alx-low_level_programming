c66e0ec4a146a491e9b8b7755dbc4d6b92973845
nemlic <nemlicmain22@gmail.com>
2023-04-15 14:50:42 +0300

    secon

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed 
+ * @argc: argument count
+ * @argv: array pointer to a string
+ * Return: 0 on sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+}
