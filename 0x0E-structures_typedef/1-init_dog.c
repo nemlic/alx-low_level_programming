8d5d674ec98b94bc2ccba509acd56e1084d0e79a
nemlic <nemlicmain22@gmail.com>
2023-04-18 20:57:37 +0300

    tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ *init_dog - initialies
+ *@name: dogs name
+ *@age: dogs age
+ *@owner: owners name
+ *@d: pointer to struture
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == '\0')
+		d = malloc(sizeof(struct dog));
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
