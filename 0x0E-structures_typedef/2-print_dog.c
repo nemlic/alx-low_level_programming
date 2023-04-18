70233473b9df8eea8da33e1cf32fe5a95c2ca9e5
nemlic <nemlicmain22@gmail.com>
2023-04-18 22:09:32 +0000

    second

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Age: %f\n", d->age);
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Owner: %s\n", d->owner);
+	printf("Age: %f\n", d->age);
 }
