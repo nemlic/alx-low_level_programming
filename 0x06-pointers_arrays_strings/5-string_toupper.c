9280faea106599bffb57fb929f49b511fdd6e148
nemlic <nemlicmain22@gmail.com>
2023-04-19 18:46:28 +0000

    nerx

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,10 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-		i++;
-	
-		if (s[i] >= 'a' && s[i] <= 'z')
+		for (i = 0; s[i] != '\0';i++)
 		{
+		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 'a' + 'A';
-		i++;
 		}
 		return (s);
 }
