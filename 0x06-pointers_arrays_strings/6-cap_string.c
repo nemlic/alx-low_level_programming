3e392a80d408673a207aa7ec747f81f77f495000
nemlic <nemlicmain22@gmail.com>
2023-04-19 19:32:21 +0000

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *cap_string - capitalises strings first letter
+  *@s: string to be capitalised
+  *Return: string
+  */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z')
+			&& (s[i - 1] == ',' ||
+			s[i - 1] == ';' ||
+			s[i - 1] == '.' ||
+			s[i - 1] == '!' ||
+			s[i - 1] == '?' ||
+			s[i - 1] == '"' ||
+			s[i - 1] == '(' ||
+			s[i - 1] == ')' ||
+			s[i - 1] == '{' ||
+			s[i - 1] == '}' ||
+			s[i - 1] == '\n' ||
+			s[i - 1] == '\t' ||
+			))
+			 s[i] = s[i] - 'a' + 'A';
+		 i++;
+	}
+	return (s);
+}
