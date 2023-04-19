3f52cade546470136277255de09449e4e9769298
nemlic <nemlicmain22@gmail.com>
2023-04-19 10:00:59 +0000

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  *
-  *
-  *
-  *
+  *_stncpy - finction that copies a string
+  *@dest: location to be printed
+  *@src: string to be copied
+  *Return: dest
   */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] < src[n] || src[i] != '\0')
+	while (i < n || src[i] != '\0')
 	{
 		dest[i] = src[i];
 
