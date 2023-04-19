3cd9cff3e3f9eb4eb91e66ba3b3b8f4928f322f2
nemlic <nemlicmain22@gmail.com>
2023-04-19 18:23:36 +0000

    nerx

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *string_toupper- changes lowercase to CAPS
+  *@s: input string
+  *Return: pointer to char
+  */
+char *string_toupper(char *)
+{
+	while (*s)
+		if (*s >= 'a' && *s <= 'z')
+			*s = *s -'a' + 'A';
+	s++;
+}
