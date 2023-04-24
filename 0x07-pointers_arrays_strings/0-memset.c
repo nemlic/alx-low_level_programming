a50adc59153035913a1a546c2c00c92af19ad114
nemlic <nemlicmain22@gmail.com>
2023-04-24 08:13:12 +0300

    first

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for(; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return(s)
+}
