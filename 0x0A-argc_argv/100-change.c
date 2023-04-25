2405aeeee79b07db0d5724fc0ef098954d71df00
nemlic <nemlicmain22@gmail.com>
2023-04-25 23:37:07 +0300

    extra

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,12 +23,12 @@ int main(int argc, char *argv[])
 	num = atoi(argv[1]);
 	result = 0;
 
-	if(num < 0)
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for(j = 0; j < 5 && num >= 0; j++)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
 		{
