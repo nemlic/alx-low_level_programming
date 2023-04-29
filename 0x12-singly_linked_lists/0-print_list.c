4c48034d4e31a8f960dbd96f4a0f1729f9943b6a
nemlic <nemlicmain22@gmail.com>
2023-04-29 02:03:20 +0000

    corrections added null

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (!h->str == NULL)
+		if (h->str == NULL)
 			printf("[0](nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
 		s++;
+		h = h->next;
 	}
 	return (s);
 }
