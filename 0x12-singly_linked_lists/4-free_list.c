92fc7e46f5ef7b9a6dd045dd2e151866aad27d5d
nemlic <nemlicmain22@gmail.com>
2023-04-29 09:48:30 +0000

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,9 +8,13 @@
   */
 void free_list(list_t *head)
 {
+	
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
-		free(head->str == NULL);
+		free(head->str);
 		free(head);
 		head = head->next;
 	}
