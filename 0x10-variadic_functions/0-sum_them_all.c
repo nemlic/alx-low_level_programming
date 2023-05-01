12a320e080a41657754708b99468beb1d394bb93
nemlic <nemlicmain22@gmail.com>
2023-05-01 17:28:55 +0300

    initial

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - adds all passed arguments
+ *@n: number of arguments passed
+ *@...:variable number of arguments to summ
+ *Return: 0 if n==0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list lists;
+
+	if (n == 0)
+		return(0);
+
+	va_start(lists, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(lists, int);
+	}
+	va_end(lists);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,17 @@
+#include "varadic_functions"
+#include <stdarg.h>
+
+/**
+ *print_numbers - prints numbers and anew line
+ *@n:
+ *@separator: syring to be printed
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	  var_start();
+	  var_arg();
+	  var_end();
+
+}
