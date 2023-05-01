cd02b0dc3410d7b3b701adfe909d80fff3aedb34
nemlic <nemlicmain22@gmail.com>
2023-05-01 19:01:22 +0300

    second

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,17 +1,26 @@
 #include "varadic_functions"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  *print_numbers - prints numbers and anew line
- *@n:
- *@separator: syring to be printed
- *
+ *@n:no of passed arguments
+ *@separator: syring to be printed btwn numbers
+ *@...: variable number of args passed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	  var_start();
-	  var_arg();
-	  var_end();
+	va_list nums;
 
+	  var_start(nums, n);
+	  for (i = 0; i < n; i++)
+	  {
+		  printf("%d", var_arg(nums int));
+		  if (i != (n-1) && separator != NULL)
+			  printf("%s", separator);
+	  }
+	  printf("\n");
+
+	  var_end(nums);
 }
