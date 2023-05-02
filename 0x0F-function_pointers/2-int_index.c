9e46c8ffb4940e1d3d519a90ee57cf69195c0efa
nemlic <nemlicmain22@gmail.com>
2023-05-02 21:30:14 +0000

    fixed return

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+  *int_index - searches for integers
+  *@size: size of the array
+  *@array:array to be searched
+  *@cmp: pointer to function
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+	{
+	int i;
+
+	if (array != NULL && cmp != NULL)
+	{
+		if (size > 0)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if ((*cmp)(array[i]))
+					return (i);
+			}
+		}
+	}
+	return (-1);
+}
