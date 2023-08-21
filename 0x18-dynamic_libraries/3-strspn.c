84d85671db73f4230868dbef0b2a74373d46d737
nemlic <nemlicmain22@gmail.com>
2023-08-21 23:42:27 +0000

    task 0

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int z = 0;
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				z++;
+				break;
+			}
+			else if (accept[x + 1] == '\0')
+				return (z);
+		}
+		s++;
+	}
+	return (z);
+}
+
+
